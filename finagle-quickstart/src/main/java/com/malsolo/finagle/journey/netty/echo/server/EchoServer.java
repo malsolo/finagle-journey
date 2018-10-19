ab9750ac5af0d3fd5df24e53378435652a9f525f
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-10-19 11:36:52 +0200

    finagle-journey: Netty.

---
+++ finagle-quickstart/src/main/java/com/malsolo/finagle/journey/netty/echo/server/EchoServer.java
@@ -0,0 +1,67 @@
+package com.malsolo.finagle.journey.netty.echo.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Try with:
+ * $ telnet localhost 8181
+ */
+@SuppressWarnings("WeakerAccess")
+public class EchoServer {
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(serverHandler);
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            // Bind and start to accept incoming connections.
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        }
+        finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port;
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        else {
+            port = 8181;
+        }
+        new EchoServer(port).start();
+    }
+
+}
