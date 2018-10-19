5c3330fac927f3debc1f40985b7ce2c6e6fd3309
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-10-19 09:50:08 +0200

    finagle-journey: composing filters and services. A filter example.
    See https://lunatech.com/blog/WDwEjiUAACQAdhdw/an-introduction-of-finagle-by-example

---
+++ finagle-quickstart/src/main/scala/com/malsolo/finagle/journey/serversandfilters/ComposingFiltersApp.scala
@@ -0,0 +1,34 @@
+package com.malsolo.finagle.journey.serversandfilters
+
+import com.malsolo.finagle.journey.quickstart.Client.client
+import com.twitter.finagle.util.DefaultTimer
+import com.twitter.finagle.{Http, Service, SimpleFilter, http}
+import com.twitter.util._
+
+class TimeoutFilter[Req, Rep](timeout: Duration, timer: Timer)
+  extends SimpleFilter[Req, Rep] {
+
+  def apply(request: Req, service: Service[Req, Rep]): Future[Rep] = {
+    val res = service(request)
+    res.within(timer, timeout)
+  }
+}
+
+object ComposingFiltersApp extends App {
+  val client = Http.newService("localhost:8282")
+  val timeoutFilter = new TimeoutFilter[http.Request, http.Response](Duration.fromMilliseconds(1000), new JavaTimer(false))
+  val clientWithTimeoutFilter = timeoutFilter andThen client
+
+  val request = http.Request(http.Method.Get, "/")
+
+  val response: Future[http.Response] = clientWithTimeoutFilter(request)
+
+  Await.result(response.onSuccess { rep: http.Response =>
+    println("GET success: " + rep)
+  })
+
+  Thread.sleep(250)
+  println("HTTP Client. Done.")
+
+}
+
